1bae21f08b4edf75e3ba18df0c6a9f466a1b5284
Marco <marcoille94@gmail.com>
2020-05-10 14:09:56 +0200

    Added enviroment.yml

--- crashes_heat_maps.py
+++ crashes_heat_maps.py
@@ -1,10 +1,11 @@
-# import webbrowser
 import pandas as pd
 import folium
 from folium.plugins import HeatMap, HeatMapWithTime
 from folium import plugins
 
 # %%
+# Setting up constants common for all maps
+
 crashes = pd.read_csv("./data/crashes_2019_regions.csv")
 chi_bounding = {
     "lon": -87.6298,
@@ -24,7 +25,6 @@ heat_data = [[row['LATITUDE'], row['LONGITUDE']]
 
 HeatMap(data=heat_data, max_zoom=20, radius=12).add_to(CHI_map)
 CHI_map.save("./web/folium/heat_crashes.html")
-# webbrowser.open("file:///Users/mi/soviz_project/maps/chi_heat.html", new=2)
 
 # %%
 # Heat map over time - crashes
@@ -34,8 +34,6 @@ heat_df = crashes.loc[:, ['LATITUDE', 'LONGITUDE', 'CRASH_DATE']].dropna()
 # Create weight column, using date
 heat_df['Weight'] = pd.to_datetime(heat_df['CRASH_DATE']).dt.hour
 heat_df['Weight'] = heat_df['Weight'].astype(float).dropna()
-# heat_df = heat_df.dropna(axis=0, subset=['Y','X', 'Weight'])
-
 
 # List comprehension to make out list of lists
 heat_data = [[[row['LATITUDE'], row['LONGITUDE']] for _, row in heat_df[heat_df['Weight'] == i].iterrows()] for i in
@@ -43,20 +41,18 @@ heat_data = [[[row['LATITUDE'], row['LONGITUDE']] for _, row in heat_df[heat_df[
 
 # Plot it on the map
 hm = plugins.HeatMapWithTime(heat_data,
-                             auto_play=True,
+                             auto_play=False,
                              radius=5,
                              position="topright"
                              )
 hm.add_to(CHI_map_time)
 CHI_map_time.save("./web/folium/heat_crashes_over_time.html")
 
-
 # %%
 # Static heat map fatal
 CHI_map = folium.Map(map_location, tiles="Stamen Toner", zoom_start=map_zoom)
 incapacitating_injuries = crashes["INJURIES_INCAPACITATING"] > 0
 fatal_injuries = crashes["INJURIES_FATAL"] > 0
-# data = crashes[incapacitating_injuries_mask]
 data = crashes[fatal_injuries]
 points = data.loc[:, ['LATITUDE', 'LONGITUDE']].dropna()
 heat_data = [[row['LATITUDE'], row['LONGITUDE']]
@@ -64,7 +60,6 @@ heat_data = [[row['LATITUDE'], row['LONGITUDE']]
 
 HeatMap(data=heat_data, max_zoom=15, radius=25).add_to(CHI_map)
 CHI_map.save("./web/folium/heat_fatal.html")
-# webbrowser.open("file:///Users/mi/soviz_project/maps/chi_heat.html", new=2)
 
 # %%
 # Static heat map incapacitating
@@ -76,7 +71,6 @@ heat_data = [[row['LATITUDE'], row['LONGITUDE']]
 
 HeatMap(data=heat_data, max_zoom=15, radius=13).add_to(CHI_map)
 CHI_map.save("./web/folium/heat_incapacitating.html")
-# webbrowser.open("file:///Users/mi/soviz_project/maps/chi_heat.html", new=2)
 
 # %%
 # Static heat map fatal and incapacitating
@@ -89,4 +83,3 @@ heat_data = [[row['LATITUDE'], row['LONGITUDE']]
 
 HeatMap(data=heat_data, max_zoom=14, radius=12).add_to(CHI_map)
 CHI_map.save("./web/folium/heat_fatal_and_incapacitating.html")
-# webbrowser.open("file:///Users/mi/soviz_project/maps/chi_heat.html", new=2)
