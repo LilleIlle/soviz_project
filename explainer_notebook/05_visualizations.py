78428e071debf1be0b5109dfcf60d88e467327f0
Robert Karlsson <s143321@student.dtu.dk>
2020-05-15 21:04:13 +0200

    Fix typos

--- explainer_notebook/05_visualizations.py
+++ explainer_notebook/05_visualizations.py
@@ -1,15 +1,15 @@
 # %% [markdown]
 
 # # 5. Visualizations
-# Almost all charts are bar charts and histograms.
+# Bar charts, histograms and maps are the primary type of visualizations used.
 # Bar charts are chosen for their superior readability—they convey categorized quantities concisely.
-# Conversely, abominations such as pie and donut charts are _not_ used.
+# Conversely, charts such as pie and donut charts are _not_ used.
 # The goal has been to keep the data–ink ratio [Tufte] high and the color scheme consistent throughout the project.
 #
-# In Figure X of Section 3 a twin y-axis chart is used to combine a histogram with a trend line.
+# In the figure in Section 3 showing the plot of number of crashes and average speeds a twin y-axis chart is used to combine a histogram with a trend line.
 # The striking red color of the trend line is also used for coloring the y-axis itself, rendering legends surplus.
 #
-# In Figure X of Section 3 a scatter chart is used to investigate the correlation between two attributes.
+# In Section 3 a scatter chart is used to investigate the correlation between two attributes.
 #
 # For geospatial data, map visualizations are naturally used to convey the locality of crashes as well
 # as heat maps showing the concentration of various events. For showing the temporal development of crashes,
--- explainer_notebook/06_discussion.py
+++ explainer_notebook/06_discussion.py
@@ -4,18 +4,18 @@
 #
 # ### What went well
 # The source of visualizations is obviously data. As this assignment basically started in the search of a data set,
-# we spend a fair amount of time seeking out a data set of high quality and not just seemed interesting on the surface.
+# we spend a fair amount of time seeking out a data set of high quality and not just one that seemed interesting on the surface.
 # Eventually, we found the Chicago data sets that seemed promising. This was indeed the case, as we found the data sets
-# to be rich and well-groomed with few missing data points and many interesting attributes, more than had time to cover in
-# this project. This in turn resulted in a very smooth data cleaning and pre-processing phase that quickly launched us into
-# doing actual visualization work. Hence, taking the time to research a promising data was likely a good use of our time on this project.
-# For the visualizations, we have had a focus on temporal plots and animations which has shown interesting patterns of traffic
-# in different regions of the city through-out the day, month and the grander scheme of a whole year. Furthermore, the focus on
-# primary causes for traffic accidents enabling us to see which causes are most common on different times of the day.
+# to be rich and well-groomed with few missing data points and many interesting attributes—more than we had time to cover in
+# this project. This in turn resulted in a smooth data cleaning and pre-processing phase that quickly launched us into
+# doing actual visualization work. Hence, taking the time to research a promising data set was likely a good use of time in the initial phase of this project.
+# For the visualizations, we have had a focus on temporal plots and animations which have shown interesting patterns of traffic
+# in different regions of the city throughout the day, month and the grander scheme of a whole year. Furthermore, the focus on
+# primary contributory causes for traffic accidents enabled us to see which causes are most common on different times of the day.
 #
 # ### What is still missing? What could be improved? Why?
 #
-# At the outset of this project on of our main goals was to make a quantified assessment of how dangerous
+# At the outset of this project one of our main goals was to make a quantified assessment of how dangerous
 # the streets of Chicago is in terms of its regions and time of day, e.g. to state the probability of a
 # crash given location and time.
 #
@@ -30,16 +30,14 @@
 # passing any given location at any given time. Such data is _not_ available for the City of Chicago, if anywhere.
 #
 # Instead, det Congestion data set provides estimates of the traffic congestion in terms of average speed of public
-# transportation buses
-# a lower average speed indicates a higher level of congestion.
+# transportation buses—a lower average speed indicates a higher level of congestion.
 # It is—reasonably—assumed that a higher level of congestion means a higher amount of vehicles on the streets.
 # However, the average speed of buses is not only affected by other vehicles taking up space on the streets.
-# It is also affected by the number of commuters utilizing them
-# more commuters means the buses spend more time at each
+# It is also affected by the number of commuters utilizing them—more commuters means the buses spend more time at each
 # bus stop, pushing the average speed downwards.
 #
 # Though not the perfect solution, for this project traffic congestion where used as a measure for the amount of traffic.
 #
-# For future work, it could be interesting to access the amount of traffic more precisely, to be able to precisely visualize and assess which
-# parts of the city has the highest change of a crash relatively to the amount of traffic. This would ultimately require a different means of
+# For future work, it could be interesting to assess the amount of traffic more precisely, to be able to precisely visualize and assess which
+# parts of the city has the highest probability of a crash relatively to the amount of traffic. This would ultimately require a different means of
 # gathering the data, e.g. via satellite photos.
